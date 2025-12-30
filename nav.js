31379c56f407225eb9b85eab041aeb73395b881c
karam faris <karam2004fares@gmail.com>
2025-12-30 11:52:50 +0300

    Delete nav.js

--- nav.js
+++
@@ -1,9 +0,0 @@
-function locToSignUp() {
-    window.location.href = "signUp.html";
-}
-function locToHsPage() {
-    window.location.href = "mvpHsCode.html";
-}
-function locToLogin() {
-    window.location.href = "logIn.html";
-}
