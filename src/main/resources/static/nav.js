c751c23d9e1e6325178700dbc236515d78838c36
ahmedyasien04 <ahmedlwama244@gmail.com>
2026-01-08 03:51:45 +0300

    Delete src/main/resources/static/nav.js

--- src/main/resources/static/nav.js
+++
@@ -1,9 +0,0 @@
-function locToSignUp() {
-    window.location.href = "signup.html";
-}
-function locToHsPage() {
-    window.location.href = "hscode.html";
-}
-function locToLogin() {
-    window.location.href = "login.html";
-}
